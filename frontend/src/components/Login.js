6acc33d40fda2d3640072b606d341a120f3d694f
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-12-20 00:24:42 +0530

    inital commit

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Login = () => {
+  const login = () => {};
+
+  const handleInputChange = ({ login }) => {};
+  return (
+    <div
+      className="m-auto w-50 bg-dark shadow-lg rounded d-flex align-items-center justify-content-center"
+      style={{ height: "80vh" }}
+    >
+      <div className="submit-form w-75">
+        <div>
+          <div className="form-group">
+            <label htmlFor="user" className="text-white mb-3 mt-3">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control py-2"
+              id="name"
+              required
+              placeholder="Enter your username"
+              // value={user.name}
+              onChange={handleInputChange}
+              name="name"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="id" className="text-white mb-3 mt-3">
+              ID
+            </label>
+            <input
+              type="text"
+              className="form-control py-2"
+              id="id"
+              required
+              placeholder="Enter your password"
+              // value={user.id}
+              onChange={handleInputChange}
+              name="id"
+            />
+          </div>
+
+          <button onClick={login} className="py-2 btn btn-success mt-5 w-100">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
